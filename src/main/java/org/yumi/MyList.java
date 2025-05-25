c57055f3276b63e86b26860d4b8d913bc0e76e8a
yumi <835094257@qq.com>
2025-05-26 03:16:03 +0800

    play happy

--- src/main/java/org/yumi/MyList.java
+++ src/main/java/org/yumi/MyList.java
@@ -8,9 +8,7 @@ import java.util.List;
 public class MyList {
     public static void main(String[] args) {
         List<Person> list = new ArrayList<>();
-        list.add(new Person("c", "3"));
-        list.add(new Person("a", "1"));
-        list.add(new Person("b", "2"));
+
         Collections.sort(list, Comparator.comparing(s -> s.age));
         for (Person p : list) {
             System.out.println(p.age);
